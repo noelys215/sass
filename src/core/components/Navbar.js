3e774bf1e1898852344ada2d819ea643a48da6eb
Henry B <noelys215@gmail.com>
2023-06-30 16:45:44 -0400

    FEAT: Add Layout and Navbar components

--- src/core/components/Navbar.js
+++ src/core/components/Navbar.js
@@ -1,1 +1,20 @@
-export default function Navbar() {}
+import Link from 'next/link';
+import Logo from 'src/core/components/Logo';
+
+export default function Navbar() {
+	return (
+		<div className="nav-container border-b-2 border-black">
+			<Link href={'/'}>
+				<Logo />
+			</Link>
+			<div className="nav-menu">
+				<Link className="nav-link white" href={'/login'}>
+					<div>Login</div>
+				</Link>
+				<Link className="nav-link black" href={'/pricing'}>
+					<div>Pricing</div>
+				</Link>
+			</div>
+		</div>
+	);
+}
