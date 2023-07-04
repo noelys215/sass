bbd09dd35c3fb9f0050184dc88a773b030002d9d
Henry B <noelys215@gmail.com>
2023-07-03 20:27:49 -0400

    FEAT: Start individual product page

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,1 +1,31 @@
-export default function ProductPage() {}
+import { supabase } from 'supabase';
+
+export default function ProductPage({ product }) {
+	return <div></div>;
+}
+
+export async function getStaticPaths() {
+	const { data: products } = await supabase.from('product').select('slug');
+
+	const paths = products.map((product) => ({
+		params: {
+			slug: product.slug,
+		},
+	}));
+
+	return {
+		paths,
+		fallback: false,
+	};
+}
+export async function getStaticProps(context) {
+	const slug = context.params.slug;
+
+	const { data: product } = await supabase.from('product').select('*').eq('slug', slug).single();
+
+	return {
+		props: {
+			product,
+		},
+	};
+}
